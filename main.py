0266d1877b8e30568392127c006454808a827634
alimobarki2030 <alimobarki.ad@gmail.com>
2025-06-14 17:37:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine, Base
 from routes.auth_routes import router as auth_router
-from routes.google_auth import router as google_router
 from routes.analytics_routes import router as analytics_router
 from routes.ga4 import router as ga4_router
 from dotenv import load_dotenv
@@ -26,9 +25,6 @@ app.add_middleware(
 
 # ✅ إضافة المسارات (routers)
 app.include_router(auth_router)
-app.include_router(google_router)
-app.include_router(analytics_router)
-app.include_router(ga4_router)
 
 # ✅ مسار الجذر
 @app.get("/")
