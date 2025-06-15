02a2f6be85ea1ccd69b4c192cbd7c14fc25f1ab2
alimobarki2030 <alimobarki.ad@gmail.com>
2025-06-15 11:20:19 +0300

    🔧 تحديث باكند: حذف Google Analytics وتحسين التسجيل

--- routes/auth.py
+++
@@ -1,5 +0,0 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from utils import get_current_user
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
