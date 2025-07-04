e25f9c573a1f82c938dd13e7780dca4fbc523e0a
alimobarki2030 <alimobarki.ad@gmail.com>
2025-07-04 21:17:08 +0300

    تعديل لزوهو

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,10 @@
 # models/__init__.py
 from app.database import Base
 from .user import User
+from .pending_store import PendingStore
 
 try:
     from .salla import SallaStore, SallaProduct
-    __all__ = ["Base", "User", "SallaStore", "SallaProduct"]
+    __all__ = ["Base", "User", "SallaStore", "SallaProduct", "PendingStore"]
 except ImportError:
-    __all__ = ["Base", "User"]
+    __all__ = ["Base", "User", "PendingStore"]
