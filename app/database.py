4e788aa41f336e870a4ad15f42047b8f9520decb
alimobarki2030 <alimobarki.ad@gmail.com>
2025-07-15 14:16:33 +0300

    تعديل قاعدة البيانات

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./breevo.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app/breevo.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 
